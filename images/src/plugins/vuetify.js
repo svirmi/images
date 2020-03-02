92114e33db552dcfe7c982d42c9a6c2fe5aa30c8
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-03-02 08:41:19 +0200

    icons

--- images/src/plugins/vuetify.js
+++ images/src/plugins/vuetify.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import '@mdi/font/css/materialdesignicons.css'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
 
-const opts = {}
+const opts = {
+    icons: {
+        iconfont: 'mdi' //'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+    },
+}
 
 export default new Vuetify(opts)
