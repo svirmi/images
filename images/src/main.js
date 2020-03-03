0844ffa4729e59539510570ad213118ab4ce6810
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-03-03 09:12:12 +0200

    Vuex plugged in

--- images/src/main.js
+++ images/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import vuetify from '@/plugins/vuetify'
 import App from './App.vue'
+import store from './store';
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   vuetify,
+  store,
 }).$mount('#app')
