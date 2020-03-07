faa36ab1d0e9fdb0fc944a05d74a140061ac66d2
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-03-07 16:29:23 +0200

    wire up store into the app

--- images/src/store/modules/auth.js
+++ images/src/store/modules/auth.js
@@ -1,3 +1,5 @@
+import api from '../../api/imgur';
+
 const state = {
     token: null
 };
@@ -7,6 +9,10 @@ const getters = {
 };
 
 const actions = {
+    login: () => {
+      api.login();
+    },
+
     logout: ({ commit }) => {
         commit('setToken', null);
     }
@@ -17,3 +23,10 @@ const mutations = {
         state.token = token;
     }
 };
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
