0a37b8c8ea29d6726f7dac0d7bbac334f7c6c709
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-03-07 15:00:55 +0200

    auth: state, getters, actions, mutations

--- images/src/store/modules/auth.js
+++ images/src/store/modules/auth.js
@@ -1,7 +1,19 @@
-const state = {};
+const state = {
+    token: null
+};
 
-const getters = {};
+const getters = {
+    isLoggedIn: state => !!state.token
+};
 
-const actions = {};
+const actions = {
+    logout: ({ commit }) => {
+        commit('setToken', null);
+    }
+};
 
-const mutations = {};
+const mutations = {
+    setToken: (state, token) => {
+        state.token = token;
+    }
+};
