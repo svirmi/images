0331c8f724b157de5ed704b161f582844ee50172
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-03-14 12:47:31 +0200

    VueRouter wired to the app

---
+++ images/src/components/AuthHandler.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>Please wait...</div>
+</template>
+
+<script>
+    export default {
+        name: "AuthHandler"
+    }
+</script>
--- images/src/main.js
+++ images/src/main.js
@@ -1,12 +1,27 @@
 import Vue from 'vue';
+import VueRouter from "vue-router";
 import vuetify from '@/plugins/vuetify';
 import App from './App.vue';
 import store from './store';
+import AuthHandler from "./components/AuthHandler";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/oauth2/callback',
+      component: AuthHandler
+    }
+  ]
+});
+
 new Vue({
-  render: h => h(App),
   vuetify,
   store,
+  router,
+  render: h => h(App),
 }).$mount('#app');
