24ed38092c50abd455202a1c47be3159b1847b11
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-03-14 15:44:58 +0200

    HTML formatting

--- images/src/components/AppHeader.vue
+++ images/src/components/AppHeader.vue
@@ -125,36 +125,22 @@
         >
             <v-icon>mdi-plus</v-icon>
         </v-btn>
-        <v-dialog
-                v-model="dialog"
-                width="800px"
-        >
+        <v-dialog v-model="dialog" width="800px">
             <v-card>
                 <v-card-title class="grey darken-2">
                     Create contact
                 </v-card-title>
                 <v-container>
                     <v-row class="mx-2">
-                        <v-col
-                                class="align-center justify-space-between"
-                                cols="12"
-                        >
-                            <v-row
-                                    align="center"
-                                    class="mr-0"
-                            >
-                                <v-avatar
-                                        size="40px"
-                                        class="mx-3"
-                                >
+                        <v-col class="align-center justify-space-between" cols="12">
+                            <v-row align="center" class="mr-0">
+                                <v-avatar size="40px" class="mx-3">
                                     <img
                                             src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
                                             alt=""
                                     >
                                 </v-avatar>
-                                <v-text-field
-                                        placeholder="Name"
-                                />
+                                <v-text-field placeholder="Name"/>
                             </v-row>
                         </v-col>
                         <v-col cols="6">
