8fc24bc9084dc5cea4f26a08b9fafbfe30ce24b2
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-03-14 16:37:25 +0200

    dialog popup removed

--- images/src/components/AppHeader.vue
+++ images/src/components/AppHeader.vue
@@ -89,12 +89,7 @@
 
             <div v-if="isLoggedIn">
                 <v-btn icon>
-                    <v-icon>mdi-bell</v-icon>
-                </v-btn>
-                <v-btn icon large>
-                    <v-avatar size="32px" item>
-                        <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify"/>
-                    </v-avatar>
+                    <v-icon>mdi-image</v-icon>
                 </v-btn>
                 <v-btn icon @click="logout">
                     <v-icon>mdi-logout</v-icon>
@@ -114,85 +109,16 @@
                 </v-row>
             </v-container>
         </v-content>
-        <v-btn
+        <v-btn v-if="isLoggedIn"
                 bottom
                 color="pink"
                 dark
                 fab
                 fixed
                 right
-                @click="dialog = !dialog"
         >
             <v-icon>mdi-plus</v-icon>
         </v-btn>
-        <v-dialog v-model="dialog" width="800px">
-            <v-card>
-                <v-card-title class="grey darken-2">
-                    Create contact
-                </v-card-title>
-                <v-container>
-                    <v-row class="mx-2">
-                        <v-col class="align-center justify-space-between" cols="12">
-                            <v-row align="center" class="mr-0">
-                                <v-avatar size="40px" class="mx-3">
-                                    <img
-                                            src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                                            alt=""
-                                    >
-                                </v-avatar>
-                                <v-text-field placeholder="Name"/>
-                            </v-row>
-                        </v-col>
-                        <v-col cols="6">
-                            <v-text-field
-                                    prepend-icon="mdi-account-card-details-outline"
-                                    placeholder="Company"
-                            />
-                        </v-col>
-                        <v-col cols="6">
-                            <v-text-field
-                                    placeholder="Job title"
-                            />
-                        </v-col>
-                        <v-col cols="12">
-                            <v-text-field
-                                    prepend-icon="mdi-mail"
-                                    placeholder="Email"
-                            />
-                        </v-col>
-                        <v-col cols="12">
-                            <v-text-field
-                                    type="tel"
-                                    prepend-icon="mdi-phone"
-                                    placeholder="(000) 000 - 0000"
-                            />
-                        </v-col>
-                        <v-col cols="12">
-                            <v-text-field
-                                    prepend-icon="mdi-text"
-                                    placeholder="Notes"
-                            />
-                        </v-col>
-                    </v-row>
-                </v-container>
-                <v-card-actions>
-                    <v-btn
-                            text
-                            color="primary"
-                    >More</v-btn>
-                    <v-spacer />
-                    <v-btn
-                            text
-                            color="primary"
-                            @click="dialog = false"
-                    >Cancel</v-btn>
-                    <v-btn
-                            text
-                            @click="dialog = false"
-                    >Save</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
     </v-app>
 </template>
 
@@ -211,7 +137,6 @@
             source: String,
         },
         data: () => ({
-            dialog: false,
             drawer: null,
             items: [
                 { icon: 'mdi-contacts', text: 'Contacts' },
