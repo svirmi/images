3c350c9e56750afe7d97fcc8138004ed8a2b1c8c
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-03-17 09:30:09 +0200

    fecthImages code

--- images/src/api/imgur.js
+++ images/src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from 'qs';
+import axios from 'axios';
 
 const CLIENT_ID = process.env.VUE_APP_CLIENT_ID;
 const ROOT_URL = 'https://api.imgur.com';
@@ -9,6 +10,7 @@ const ROOT_URL = 'https://api.imgur.com';
 */
 
 export default {
+
     login() {
         const querystring = {
             client_id: CLIENT_ID,
@@ -16,5 +18,14 @@ export default {
         };
 
         window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
+    },
+
+    fetchImages(token) {
+        const url = `${ROOT_URL}/3/account/me/images`;
+        const headers = {
+            Authorization: `Bearer ${token}`
+        };
+
+        return axios.get(url,headers);
     }
 }
