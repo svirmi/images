ece3b8a5b635d2f8c249c5e3b21806c23c3e86ea
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-03-23 09:12:34 +0200

    styling dropzone input

--- images/src/components/UploadForm.vue
+++ images/src/components/UploadForm.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        Upload form
+    <div class="dropper">
+        <input type="file" />
+        <span>Drag files here!</span>
     </div>
 </template>
 
@@ -11,5 +12,20 @@
 </script>
 
 <style scoped>
-
+    .dropper {
+        height: 30vh;
+        width: 30%;
+        border: 2px dashed black;
+        border-radius: 5px;
+        position: relative;
+    }
+    .dropper:hover{
+        background-color: darkgray;
+    }
+    input {
+        height: 30vh;
+        position: absolute;
+        width: 100%;
+        opacity: 0;
+    }
 </style>
